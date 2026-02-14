08307ec6097384a3a2d22e6a416166d681612f93
Manvi V Rai <manvivrai@gmail.com>
2026-02-14 22:46:39 +0530

    Added MongoDB config, models and auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("QuizQuest Backend is running 🚀");
+  res.send("QuizQuest Backend Running 🚀");
 });
 
 const PORT = process.env.PORT || 5000;
